f68620ea4f43e428200697bd99ba742fc1367012
Mustafa Balkaya <balkayamustafa99@gmail.com>
2024-05-06 03:33:53 +0300

    refactor: create layout

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import index,contact
 urlpatterns = [
     path('', index, name='index'),
-    path('contact', contact, name='index'),
+    path('contact', contact, name='contact'),
 ]
