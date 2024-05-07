4f03ad75736f32b42b241afd2d8faf95dc431059
Mustafa Balkaya <balkayamustafa99@gmail.com>
2024-05-07 15:17:21 +0300

    feat: use gunicorn

--- core/views.py
+++ core/views.py
@@ -3,17 +3,22 @@ from core.models import GeneralSetting, ImageSetting, Skill, Experience, Educati
 
 
 # Create your views here.
+
 def get_general_setting(parameter):
     try:
-     obj = GeneralSetting.objects.get(name=parameter).parameter
+        obj = GeneralSetting.objects.get(name=parameter).parameter
     except:
         obj = ''
+
     return obj
+
+
 def get_image_setting(parameter):
     try:
-     obj = ImageSetting.objects.get(name=parameter).file
+        obj = ImageSetting.objects.get(name=parameter).file
     except:
         obj = ''
+
     return obj
 
 
@@ -24,47 +29,47 @@ def layout(request):
     home_banner_name = get_general_setting('home_banner_name')
     home_banner_title = get_general_setting('home_banner_title')
     home_banner_description = get_general_setting('home_banner_description')
-    about_myself_footer = get_general_setting('about_myself_footer')
     about_myself_welcome = get_general_setting('about_myself_welcome')
-    social_medias = SocialMedia.objects.all().order_by()
+    about_myself_footer = get_general_setting('about_myself_footer')
+
+    # Images
     header_logo = get_image_setting('header_logo')
     home_banner_image = get_image_setting('home_banner_image')
     site_favicon = get_image_setting('site_favicon')
+
+    social_medias = SocialMedia.objects.all()
     documents = Document.objects.all()
 
     context = {
-        'documents': documents,
         'site_title': site_title,
         'site_keywords': site_keywords,
         'site_description': site_description,
         'home_banner_name': home_banner_name,
         'home_banner_title': home_banner_title,
         'home_banner_description': home_banner_description,
-        'about_myself_footer': about_myself_footer,
         'about_myself_welcome': about_myself_welcome,
+        'about_myself_footer': about_myself_footer,
         'header_logo': header_logo,
         'home_banner_image': home_banner_image,
         'site_favicon': site_favicon,
+        'documents': documents,
         'social_medias': social_medias,
-
     }
     return context
 
 
 def index(request):
+    # Skills
     skills = Skill.objects.all()
 
     experiences = Experience.objects.all().order_by('-start_date')
-
     educations = Education.objects.all().order_by('-start_date')
-    context = {
 
+    context = {
         'skills': skills,
         'experiences': experiences,
         'educations': educations,
-
     }
-
     return render(request, 'index.html', context=context)
 
 
